4c92aba9910d2f66372d955b42ff050b5c5eb956
Well <welin7uelf@gmail.com>
2019-07-05 21:43:23 -0300

    incluído comentário no controller.

---
+++ ExampleSalesmanWebApi/Controllers/VendedoresController.cs
@@ -0,0 +1,34 @@
+﻿using ExampleSalesmanWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExampleSalesmanWebApi.Controllers
+{
+    public class VendedoresController : ApiController
+    {
+        private static List<Vendedores> ListaVendedores = new List<Vendedores>();
+
+        //metodo get
+        public List<Vendedores> Get()
+        {
+            return ListaVendedores;
+        }
+
+        //metodo post
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+                ListaVendedores.Add(new Vendedores(nome));
+        }
+
+        //metodo delete
+        public void Delete(string nome)
+        {
+            ListaVendedores.RemoveAt(ListaVendedores.IndexOf(ListaVendedores.First(c => c.Nome.Equals(nome))));
+        }
+    }
+}
---
+++ ExampleSalesmanWebApi/Models/Vendedores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExampleSalesmanWebApi.Models
+{
+    public class Vendedores
+    {
+        public string Nome { get; set; }
+
+        public Vendedores(string Nome)
+        {
+            this.Nome = Nome;
+        }
+    }
+}
